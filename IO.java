677abca90575a66392108b6716461e51b893e86c
Jackson Kolb <JacksonKolb94@gmail.com>
2017-07-24 14:52:50 -0600

    Fixed Death of Player and Monster
    The logic in the battle method of the IO class needed to be corrected.  Now it correctly displays when the monster dies, or when the player dies.

--- IO.java
+++ IO.java
@@ -14,10 +14,10 @@ public final class IO {
     }
 
     public static void Welcome() {
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("Welcome traveler.......");
         System.out.println("To the mines of Moria! ");
         System.out.println("-----------------------");
@@ -41,10 +41,10 @@ public final class IO {
     }
 
     public static void newGameIntroduction() {
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("------------------");
         System.out.println("CHOOSE A CHARACTER");
         System.out.println("------------------");
@@ -65,128 +65,128 @@ public final class IO {
         System.out.println();
         System.out.println();
     }
-    public static boolean displayPlayerStats(String name, String description, 
-            int maxHitPoints, int minDamage, int maxDamage, int defense, 
-            double critChance){
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    public static boolean displayPlayerStats(String name, String description,
+            int maxHitPoints, int minDamage, int maxDamage, int defense,
+            double critChance) {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println(name);
         System.out.println("-------");
         System.out.println(description);
         System.out.println();
         System.out.println("MAX HP: " + maxHitPoints);
         System.out.println();
-        System.out.println( 
+        System.out.println(
                 "ATTACK: " + minDamage + "-" + maxDamage);
         System.out.println();
         System.out.println("DEFENSE(Scale:1-5): " + defense);
         System.out.println();
         System.out.println("CRIT CHANCE: " + critChance + "%");
         System.out.println();
-        System.out.println("ARE YOU SURE YOU WANT TO PLAY AS A " + 
-                name.toUpperCase() + "? "
+        System.out.println("ARE YOU SURE YOU WANT TO PLAY AS A "
+                + name.toUpperCase() + "? "
                 + "(y/n)");
         System.out.println();
         System.out.println();
         System.out.println();
-        if(ProjectMoria.USERINPUT.nextLine().equals("y")){
+        if (ProjectMoria.USERINPUT.nextLine().equals("y")) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    public static void credits(){
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    public static void credits() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("This game was created by Jackson Kolb.");
         System.out.println("Would you like to return to the main menu? (y/n)");
-        if(ProjectMoria.USERINPUT.nextLine().equals("y")){
+        if (ProjectMoria.USERINPUT.nextLine().equals("y")) {
             ProjectMoria.mainMenu();
         }
     }
-    
-    public static void movePlayer(Player player){
-       
-        if(Dungeon.isNorthDirection() == true){
+
+    public static void movePlayer(Player player) {
+
+        if (Dungeon.isNorthDirection() == true) {
             System.out.println("North (n)\n");
-        }        
-        if(Dungeon.isSouthDirection() == true){
+        }
+        if (Dungeon.isSouthDirection() == true) {
             System.out.println("South (s)\n");
         }
-        if(Dungeon.isEastDirection() == true){
+        if (Dungeon.isEastDirection() == true) {
             System.out.println("East (e)\n");
         }
-        if(Dungeon.isWestDirection() == true){
+        if (Dungeon.isWestDirection() == true) {
             System.out.println("West (w)\n");
         }
-       
+
         System.out.print("Where would you like to travel?: ");
         String selection = ProjectMoria.USERINPUT.nextLine();
-        if(selection.equals("n") && Dungeon.isNorthDirection()){
-            player.setCurrY(player.getCurrY() + 1) ;
-        }
-        else if(selection.equals("s") && Dungeon.isSouthDirection()){
+        if (selection.equals("n") && Dungeon.isNorthDirection()) {
+            player.setCurrY(player.getCurrY() + 1);
+        } else if (selection.equals("s") && Dungeon.isSouthDirection()) {
             player.setCurrY(player.getCurrY() - 1);
-        }
-        else if (selection.equals("e") && Dungeon.isEastDirection()){
+        } else if (selection.equals("e") && Dungeon.isEastDirection()) {
             player.setCurrX(player.getCurrX() + 1);
-        }   
-        else if (selection.equals("w") && Dungeon.isWestDirection()){
+        } else if (selection.equals("w") && Dungeon.isWestDirection()) {
             player.setCurrX(player.getCurrX() - 1);
         }
-        
+
     }
-    
-    public static void battleIntro(Player player, Room room){
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    public static void battleIntro(Player player, Room room) {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("You arrive at Room [" + player.getCurrX() + "]["
+                + player.getCurrY() + "]");
         System.out.println("You enter the room and look around and see...");
         System.out.println(room.getDescription() + "\n\n");
         System.out.println("Number of monsters: " + room.getNumOfMonsters());
-        System.out.println("Your fight with " + room.getMonster().getName() 
+        System.out.println("Your fight with " + room.getMonster().getName()
                 + " begins.\n");
     }
-   
-    public static void battle(Player player, Monster monster){
-        while(player.isAlive() && monster.isAlive()){
-            System.out.println("\nMonster HP: " +  monster.getHitPoints() 
-                + "    " + "Player HP: " + player.getHitPoints());
+
+    public static void battle(Player player, Monster monster) {
+        while (player.isAlive() && monster.isAlive()) {
+            System.out.println("\nMonster HP: " + monster.getHitPoints()
+                    + "    " + "Player HP: " + player.getHitPoints());
             System.out.println("----------------------------------");
             System.out.print("\nAttack (a)");
             String action = ProjectMoria.USERINPUT.nextLine();
-            if(action.equals("a")){
+            if (action.equals("a")) {
                 monster.defend(player);
-                
+
             }
-            if(monster.isAlive()){
+            if (monster.isAlive()) {
                 player.defend(monster);
-              
+
             }
         }
-        if(!player.isAlive()){
-        
+        if (!player.isAlive()) {
+            System.out.println("Your lifeless body hits the floor.  GAME OVER");
+        } else if (!monster.isAlive()) {
+            System.out.println("The monster has been defeated!");
+            System.out.println("--------------------------------\n");
+        }
+
     }
-        System.out.println("The monster has been defeated!");
-        System.out.println("--------------------------------\n");
-    }
-    public static void playerHitPointsMessage(int damage, Monster monster){
+
+    public static void playerHitPointsMessage(int damage, Monster monster) {
         System.out.println("The " + monster.getName() + " hit you for "
-            + damage + " damage.");
+                + damage + " damage.");
     }
-    
-    public static void monsterHitPointsMessage(int damage, Monster monster){
-        System.out.println("You hit the " + monster.getName() +
-                        " for " + damage + " damage.");
+
+    public static void monsterHitPointsMessage(int damage, Monster monster) {
+        System.out.println("You hit the " + monster.getName()
+                + " for " + damage + " damage.");
     }
-    
-       public static void playerDeathMessage(){
-        System.out.println("YOU DIED.");
-    }
+
 }
