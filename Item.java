356adfb56622f2db3f58200d104ee08237881bd1
Jackson Kolb <JacksonKolb94@gmail.com>
2017-10-11 11:58:37 -0600

    Added brief comments, and 3 new types of Items
    superPotion, critPotion, and teleportPotion were all added as new Items. I also added a newRandomItem method for the future use of item drops.

--- Item.java
+++ Item.java
@@ -1,3 +1,6 @@
+/* Item Class - Handles the creation and random drops of Items in the dungeon.
+ * So far, players can use potions to heal themselves.
+*/
 package projectmoria;
 
 public class Item {
@@ -26,8 +29,41 @@ public class Item {
 
     private static Item potion() {
         return new Item("Potion", "Potion", " a small vial filled with a "
-                + "translucent red liquid");
+                + "translucent red liquid. Heals 20 HP.");
     }
+
+    private static Item superPotion() {
+        return new Item("Super Potion", "Potion", " a medium sized vial filled"
+                + " with a translucent red liquid. Heals 40 HP.");
+    }
+
+    private static Item critPotion() {
+        return new Item("Critical Hit Potion", "Potion", "a small oval shaped"
+                + " vial filled with a bubbling blue liquid. Increases crit chance "
+                + "by 10%");
+    }
+
+    private static Item teleportPotion() {
+        return new Item("Teleport Potion", "Potion", "a potion used for"
+                + "teleporting to the beginning of the dungeon.");
+    }
+
+    public static Item newRandomItem() {
+        Item randomItem = null;
+        int random = ProjectMoria.RAND.nextInt(3) + 1; //Change random value parameter to add more room types
+        switch (random) {
+            case 1:
+                randomItem = (potion());
+            case 2:
+                randomItem = (superPotion());
+            case 3:
+                randomItem = (critPotion());
+            case 4:
+                randomItem = (teleportPotion());
+        }
+        return randomItem;
+    }
+
 
     public String getName() {
         return name;
