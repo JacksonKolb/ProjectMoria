fc1aab7b2541f0c9d979d7e2e86fda4856f73dfa
Jackson Kolb <JacksonKolb94@gmail.com>
2017-10-11 11:55:17 -0600

    Added comments
    Some brief comments to explain a couple of things. The method dungeonLogic, and the general purpose of the class.

--- Dungeon.java
+++ Dungeon.java
@@ -1,6 +1,10 @@
-
+/* Jackson Kolb
+   Dungeon Class - Handles the creation of the random 30x30 array of room 
+   objects, which randomly places monster objects in each room.  This class also
+   handles the player movement logic, by determining if there are rooms to the 4
+   defined directions (north, south, east, and west).
+ */
 package projectmoria;
-
 
 public final class Dungeon {
 
@@ -8,13 +12,12 @@ public final class Dungeon {
     private static boolean southDirection = false;
     private static boolean westDirection = false;
     private static boolean eastDirection = false;
-    
+
     public static Room[][] newRandomDungeon(Player player) {
         Room[][] dungeon = new Room[30][30];
         for (int i = 0; i < dungeon.length; i++) {
             for (int j = 0; j < dungeon.length; j++) {
                 dungeon[i][j] = Room.newRoomInstance();
-
             }
         }
         player.setCurrRoom(dungeon[14][14]);
@@ -22,17 +25,17 @@ public final class Dungeon {
     }
 
     public boolean roomExists(int x, int y) {
-        return  (rowExists(x)) && (colExists(y));
+        return (rowExists(x)) && (colExists(y));
     }
 
-    public boolean rowExists(int x){
-        return  (x >= 0) && (x <= 29);
+    public boolean rowExists(int x) {
+        return (x >= 0) && (x <= 29);
     }
-    
-    public boolean colExists(int y){
-        return  (y >= 0) && (y <= 29);
+
+    public boolean colExists(int y) {
+        return (y >= 0) && (y <= 29);
     }
-    
+
     public void playerMovement(Player player) {
         northDirection = roomExists(player.getCurrX(), player.getCurrY()
                 + 1);
@@ -48,9 +51,12 @@ public final class Dungeon {
 
     public void battle(Player player, Monster monster, Room[][] dungeon) {
         IO.battleIntro(player, dungeon[player.getCurrX()][player.getCurrY()]);
-        IO.battle(player, monster);
+        IO.battle(player, monster, dungeon[player.getCurrX()][player.getCurrY()]);
     }
-
+    /* dungeonLogic - while the player is alive, and the monster in the 
+    *  current room is alive, initiate a battle. Otherwise, if the player is
+    *  alive and the monster is dead, let the player move.
+    */
     public void dungeonLogic(Player player, Room[][] dungeon) {
         while (player.isAlive()) {
             if (player.isAlive() && dungeon[player.getCurrX()][player.getCurrY()]
